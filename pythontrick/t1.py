a7d5be7e95f88c568ad1b23e9fb9606787274e00
sumangal44 <sumangalkaran44@gmail.com>
2025-12-18 08:25:18 +0530

    Add initial Python scripts for list check and f-string demonstration

---
+++ pythontrick/t1.py
@@ -0,0 +1,13 @@
+items=[]
+if len(items)==0:
+    print("The list is empty.") #❌
+
+
+
+items = []
+if not items:
+    print("The list is empty.") # ✅
+
+
+
+
---
+++ pythontrick/t2.py
@@ -0,0 +1,7 @@
+#  fstring   trick
+name:str = "desicodedaries"
+age:int = 5
+print(f"My name is {name} and I am {age} years old.")
+
+
+
